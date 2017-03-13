9162fabd85155a1b31ec13a6436e2ff44f2cc6f9
= <=>
2017-03-13 13:41:06 +0100

    TEST program now have section of pressure, temperature and software PWM

--- test/deviceTest.c
+++ test/deviceTest.c
@@ -1,10 +1,14 @@
 #include <wiringPi.h>
 #include <softPwm.h>
+#include <gertboard.h>
+#include <wiringPiSPI.h>
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <cstring>
 
 #define PWMMIN    5
 #define PWMMAX    22
@@ -14,6 +18,9 @@
 #define SERVOSOFT   2
 #define PRESSURE    3
 #define TEMPERATURE 4
+
+int myFd;
+int channel;
 
 //Usage 1 to 18
 int getPWMPot(int voltaje)
@@ -46,6 +53,44 @@ void test()
     pwmWrite(1, getPWMPot(aux));
     delay(100);
   }
+}
+
+//SPI init, must be used once
+int loadSpiDriver()
+{
+  if (system("startSPI") == -1)
+    //if (system("gpio load spi") == -1)
+  {
+    printf ("Can't load the SPI driver\n") ;
+    return -1;
+  }
+  return 0;
+}
+
+//BUS init, always required
+int spiSetup ()
+{
+  printf("Select SPI channel: ");
+  scanf("%d", &channel);
+  printf("\n");
+
+  if ((myFd = wiringPiSPISetup (channel, 1000000)) < 0)
+  {
+    printf ("Can't open the SPI bus: %s\n", strerror (errno)) ;
+    return -1;
+  }
+  return 0;
+}
+
+//Analog read
+int myAnalogRead(int analogChannel)
+{
+  if(analogChannel<0 || analogChannel>7)
+    return -1;
+  unsigned char buffer[3] = {1}; // start bit
+  buffer[1] = (analogChannel+8) << 4;
+  wiringPiSPIDataRW(channel, buffer, 3);
+  return ( (buffer[1] & 3 ) << 8 ) + buffer[2]; // get last 10 bits
 }
 
 void testServo()
@@ -101,26 +146,50 @@ void testPressure()
   scanf("%d", &pin);
   printf("\n");
 
+  pinMode(pin, INPUT);
+
   for(;;)
   {
-    printf("Pressure: ");
-    delay (500);
+    if(digitalRead(pin)) printf("Pressure HIGH\n");
+    else printf("Pressure LOW\n");
+    delay (1000);
   }
 }
 
 void testTemperature()
 {
-  int pin;
-  float temp = 0.0;
+  int pin, temp;
 
   printf("Select temperature pin: ");
   scanf("%d", &pin);
   printf("\n");
 
+  // Initialise SPI
+  if( loadSpiDriver () != 0 )
+  {
+    printf("Error loading Spi Driver\n");
+    return;
+  }
+
+  // Initialise wiringPi
+  if (wiringPiSetup () == -1)
+  {
+    printf("Error loading wiringPi\n");
+    return;
+  }
+
+  //Initialise BUS
+  if( spiSetup() != 0 )
+  {
+    printf("Error loading Spi BUS\n");
+    return;
+  }
+
   for(;;)
   {
-    printf("Temperature: %f", temp);
-    delay (500);
+    temp = myAnalogRead(pin);
+    printf("Temperature: %d\n", temp);
+    delay (1000);
   }
 }
 
@@ -136,25 +205,28 @@ int main(int argc, char *argv[])
   }
   device = atoi(argv[1]);
 
-  if (wiringPiSetup () == -1) //using wPi pin numbering
-     return (1) ;
+  if (wiringPiSetup () == -1)
+  {
+    printf("Error loading wiringPi: %s\n", strerror (errno) );
+    return -1;
+  }
 
   switch (device)
   {
     case SERVO:
       testServo();
-    break;
+      break;
     case SERVOSOFT:
       testServoSoft();
-    break;
+      break;
     case PRESSURE:
       testPressure();
-    break;
+      break;
     case TEMPERATURE:
       testTemperature();
-    break;
+      break;
     default:
-    break;
+      break;
 
   }
 
